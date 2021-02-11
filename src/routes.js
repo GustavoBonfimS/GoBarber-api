c5a7cc6d0bb69c18775666cf2f3f37a0061f97f4
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2021-02-11 18:01:15 -0300

    user controller

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/user', async (req, res) => {
-  const user = await User.create({
-    name: 'Gustavo Bonfim',
-    email: 'gustavo@teste.com',
-    password_hash: 'alsdfl',
-  });
-
-  return res.json(user);
-});
-
-routes.get('/', (req, res) => res.json({ message: 'Hello world!' }));
+routes.post('/users', UserController.store)
 
 export default routes;
