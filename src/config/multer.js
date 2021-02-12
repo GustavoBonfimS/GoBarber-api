ec12e2c12919c6236fcfd78c47224c0e8f268400
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2021-02-12 17:49:26 -0300

    multer function start

---
+++ src/config/multer.js
@@ -0,0 +1,12 @@
+import Multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+// export default {
+//   storage: Multer.diskStorage({
+//     destination: resolve(__dirname, '..', '..',, 'tmp', 'uploads'),
+//     filename: (req, file, cb) => {
+
+//     },
+//   })
+// }
