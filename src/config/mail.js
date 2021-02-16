69515c14f8a3282ce4ba45c9fc85b3ba78f22367
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2021-02-16 16:38:35 -0300

    email setup with mailTrap and nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0e70465342a7ea',
+    pass: 'd1f7538fda4218',
+  },
+  default: {
+    from: 'Equipe goBarber <noreply@gobarber.com>',
+  },
+};
