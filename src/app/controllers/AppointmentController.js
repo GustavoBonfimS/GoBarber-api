a338d68a697b862195d0a86696c649b743d95f62
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2021-02-28 22:43:17 -0300

    errro handle queue mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -110,10 +111,10 @@ class AppointmentController {
         {
           model: User,
           as: 'user',
-          atributes: ['name'],
+          attributes: ['name'],
         },
       ],
-    }); 
+    });
 
     if (appointment.user_id !== req.userId) {
       return res
@@ -132,17 +133,8 @@ class AppointmentController {
     appointment.canceled_at = new Date();
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancelation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
